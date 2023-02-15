73ffe51010bcd594661e27d32d82f913f5b49c25
JerrodMolnar <JerrodMolnar@gmail.com>
2023-02-14 21:11:07 -0500

    Added project to git

---
+++ Tic Tac Toe/frmTicTacToe.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmTicTacToe
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.lblPlayer1X = New System.Windows.Forms.Label()
+        Me.lblPlayer2O = New System.Windows.Forms.Label()
+        Me.lstWin1 = New System.Windows.Forms.ListBox()
+        Me.lstWin2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(148, 349)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(69, 32)
+        Me.btnReset.TabIndex = 0
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'lblPlayer1X
+        '
+        Me.lblPlayer1X.AutoSize = True
+        Me.lblPlayer1X.Location = New System.Drawing.Point(26, 356)
+        Me.lblPlayer1X.Name = "lblPlayer1X"
+        Me.lblPlayer1X.Size = New System.Drawing.Size(98, 19)
+        Me.lblPlayer1X.TabIndex = 1
+        Me.lblPlayer1X.Text = "Player 1: X"
+        '
+        'lblPlayer2O
+        '
+        Me.lblPlayer2O.AutoSize = True
+        Me.lblPlayer2O.Location = New System.Drawing.Point(241, 356)
+        Me.lblPlayer2O.Name = "lblPlayer2O"
+        Me.lblPlayer2O.Size = New System.Drawing.Size(97, 19)
+        Me.lblPlayer2O.TabIndex = 2
+        Me.lblPlayer2O.Text = "Player 2: O"
+        '
+        'lstWin1
+        '
+        Me.lstWin1.FormattingEnabled = True
+        Me.lstWin1.ItemHeight = 19
+        Me.lstWin1.Location = New System.Drawing.Point(13, 401)
+        Me.lstWin1.Name = "lstWin1"
+        Me.lstWin1.Size = New System.Drawing.Size(120, 80)
+        Me.lstWin1.TabIndex = 3
+        Me.lstWin1.Visible = False
+        '
+        'lstWin2
+        '
+        Me.lstWin2.FormattingEnabled = True
+        Me.lstWin2.ItemHeight = 19
+        Me.lstWin2.Location = New System.Drawing.Point(232, 401)
+        Me.lstWin2.Name = "lstWin2"
+        Me.lstWin2.Size = New System.Drawing.Size(120, 80)
+        Me.lstWin2.TabIndex = 4
+        Me.lstWin2.Visible = False
+        '
+        'frmTicTacToe
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Orange
+        Me.ClientSize = New System.Drawing.Size(364, 483)
+        Me.Controls.Add(Me.lstWin2)
+        Me.Controls.Add(Me.lstWin1)
+        Me.Controls.Add(Me.lblPlayer2O)
+        Me.Controls.Add(Me.lblPlayer1X)
+        Me.Controls.Add(Me.btnReset)
+        Me.Font = New System.Drawing.Font("Cooper Black", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 4, 6, 4)
+        Me.Name = "frmTicTacToe"
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lblPlayer1X As Label
+    Friend WithEvents lblPlayer2O As Label
+    Friend WithEvents lstWin1 As ListBox
+    Friend WithEvents lstWin2 As ListBox
+End Class
---
+++ Tic Tac Toe/frmTicTacToe.vb
@@ -0,0 +1,217 @@
+﻿Public Class frmTicTacToe
+    ' Declarations
+    Dim _intX1 As Integer = 10
+    Dim _intY1 As Integer = 10
+    Dim _intX2 As Integer = 110
+    Dim _intY2 As Integer = 110
+    Dim _intDraws As Integer = 0
+    Dim _intPlayer As Integer = 1
+    Dim _blnArray() As Boolean = {False, False, False, False, False, False, False, False, False}
+
+    Private Sub frmTicTacToe_Paint(ByVal sender As Object, ByVal e As PaintEventArgs) Handles MyBase.Paint
+        DrawBoard()
+        _intPlayer = 1
+        lblPlayer1X.ForeColor = Color.Blue
+    End Sub
+
+    Private Sub DetermineSquare(ByVal intXSquare As Integer, ByVal intYSquare As Integer)
+        _intX1 = 10
+        _intY1 = 10
+        _intX2 = 110
+        _intY2 = 110
+        _intX1 = _intX1 + intXSquare
+        _intY1 = _intY1 + intYSquare
+        _intX2 = _intX2 + intXSquare
+        _intY2 = _intY2 + intYSquare
+    End Sub
+
+    Private Sub frmTicTacToe_MouseClick(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseClick
+        Dim intSquare As Integer
+
+        intSquare = MousePos(e)
+
+        If _intPlayer = 1 Then
+            If Not lstWin1.Items.Contains(intSquare) Xor lstWin2.Items.Contains(intSquare) Then
+                If intSquare <> 0 Then
+                    lstWin1.Items.Add(intSquare)
+                    DrawX()
+                    _intDraws += 1
+                    _intPlayer = 2
+                    lblPlayer1X.ForeColor = Color.Black
+                    lblPlayer2O.ForeColor = Color.Blue
+                    FindWinnerOrBoardFull()
+                End If
+            End If
+
+        Else
+
+            If Not lstWin2.Items.Contains(intSquare) Xor lstWin1.Items.Contains(intSquare) Then
+                If intSquare <> 0 Then
+                    lstWin2.Items.Add(intSquare)
+                    DrawO()
+                    _intDraws += 1
+                    _intPlayer = 1
+                    lblPlayer1X.ForeColor = Color.Blue
+                    lblPlayer2O.ForeColor = Color.Black
+                    FindWinnerOrBoardFull()
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub DrawBoard()
+        ' Declarations
+        Dim MyPen As New Drawing.Pen(Color.Black)
+        Dim DrawGraphics As Graphics
+
+
+        DrawGraphics = Me.CreateGraphics()
+        DrawGraphics.DrawRectangle(MyPen, 10, 10, 300, 300)
+        DrawGraphics.DrawLine(MyPen, 110, 10, 110, 310)
+        DrawGraphics.DrawLine(MyPen, 210, 10, 210, 310)
+        DrawGraphics.DrawLine(MyPen, 10, 110, 310, 110)
+        DrawGraphics.DrawLine(MyPen, 10, 210, 310, 210)
+        DrawGraphics.Dispose()
+    End Sub
+
+    Private Sub DrawX()
+        ' Declarations
+        Dim MyPen As New Drawing.Pen(Color.DarkRed, 3)
+        Dim DrawGraphics As Graphics
+
+        DrawGraphics = Me.CreateGraphics()
+        DrawGraphics.DrawLine(MyPen, _intX1, _intY1, _intX2, _intY2)
+        DrawGraphics.DrawLine(MyPen, _intX2, _intY1, _intX1, _intY2)
+
+    End Sub
+
+    Private Sub DrawO()
+        ' Declarations
+        Dim MyPen As New Drawing.Pen(Color.DarkViolet, 3)
+        Dim DrawGraphics As Graphics
+
+        DrawGraphics = CreateGraphics()
+        DrawGraphics.DrawEllipse(MyPen, _intX1, _intY1, 100, 100)
+    End Sub
+
+    Private Sub ResetBoard()
+        Dim Reset As Graphics
+        Reset = Me.CreateGraphics()
+        Reset.Clear(Color.Orange)
+        lstWin1.Items.Clear()
+        lstWin2.Items.Clear()
+        DrawBoard()
+        _intPlayer = 1
+        lblPlayer1X.ForeColor = Color.Blue
+        lblPlayer2O.ForeColor = Color.Black
+    End Sub
+
+    Private Sub FindWinnerOrBoardFull()
+        ' Declarations
+        Dim MyPen As New Drawing.Pen(Color.AntiqueWhite, 3)
+        Dim DrawGraphics As Graphics
+        DrawGraphics = CreateGraphics()
+
+        If lstWin1.Items.Contains(1) And lstWin1.Items.Contains(2) And lstWin1.Items.Contains(3) Then
+            DrawGraphics.DrawLine(MyPen, 60, 10, 60, 310)
+            MsgBox("Player 1 wins")
+        ElseIf lstWin1.Items.Contains(2) And lstWin1.Items.Contains(5) And lstWin1.Items.Contains(8) Then
+            DrawGraphics.DrawLine(MyPen, 10, 160, 310, 160)
+            MsgBox("Player 1 wins")
+        ElseIf lstWin1.Items.Contains(1) And lstWin1.Items.Contains(4) And lstWin1.Items.Contains(7) Then
+            DrawGraphics.DrawLine(MyPen, 10, 60, 310, 60)
+            MsgBox("Player 1 wins")
+        ElseIf lstWin1.Items.Contains(3) And lstWin1.Items.Contains(6) And lstWin1.Items.Contains(9) Then
+            DrawGraphics.DrawLine(MyPen, 10, 260, 310, 260)
+            MsgBox("Player 1 wins")
+        ElseIf lstWin1.Items.Contains(4) And lstWin1.Items.Contains(5) And lstWin1.Items.Contains(6) Then
+            DrawGraphics.DrawLine(MyPen, 160, 10, 160, 310)
+            MsgBox("Player 1 wins")
+        ElseIf lstWin1.Items.Contains(3) And lstWin1.Items.Contains(5) And lstWin1.Items.Contains(7) Then
+            DrawGraphics.DrawLine(MyPen, 10, 310, 310, 10)
+            MsgBox("Player 1 wins")
+        ElseIf lstWin1.Items.Contains(7) And lstWin1.Items.Contains(8) And lstWin1.Items.Contains(9) Then
+            DrawGraphics.DrawLine(MyPen, 260, 10, 260, 310)
+            MsgBox("Player 1 wins")
+        ElseIf lstWin1.Items.Contains(1) And lstWin1.Items.Contains(5) And lstWin1.Items.Contains(9) Then
+            DrawGraphics.DrawLine(MyPen, 10, 10, 310, 310)
+            MsgBox("Player 1 wins")
+        ElseIf lstWin2.Items.Contains(1) And lstWin2.Items.Contains(2) And lstWin2.Items.Contains(3) Then
+            DrawGraphics.DrawLine(MyPen, 60, 10, 60, 310)
+            MsgBox("Player 2 wins")
+        ElseIf lstWin2.Items.Contains(2) And lstWin2.Items.Contains(5) And lstWin2.Items.Contains(8) Then
+            DrawGraphics.DrawLine(MyPen, 10, 160, 310, 160)
+            MsgBox("Player 2 wins")
+        ElseIf lstWin2.Items.Contains(1) And lstWin2.Items.Contains(4) And lstWin2.Items.Contains(7) Then
+            DrawGraphics.DrawLine(MyPen, 10, 60, 310, 60)
+            MsgBox("Player 2 wins")
+        ElseIf lstWin2.Items.Contains(3) And lstWin2.Items.Contains(6) And lstWin2.Items.Contains(9) Then
+            DrawGraphics.DrawLine(MyPen, 10, 260, 310, 260)
+            MsgBox("Player 2 wins")
+        ElseIf lstWin2.Items.Contains(4) And lstWin2.Items.Contains(5) And lstWin2.Items.Contains(6) Then
+            DrawGraphics.DrawLine(MyPen, 160, 10, 160, 310)
+            MsgBox("Player 2 wins")
+        ElseIf lstWin2.Items.Contains(3) And lstWin2.Items.Contains(5) And lstWin2.Items.Contains(7) Then
+            DrawGraphics.DrawLine(MyPen, 10, 310, 310, 10)
+            MsgBox("Player 2 wins")
+        ElseIf lstWin2.Items.Contains(7) And lstWin2.Items.Contains(8) And lstWin2.Items.Contains(9) Then
+            DrawGraphics.DrawLine(MyPen, 260, 10, 260, 310)
+            MsgBox("Player 2 wins")
+        ElseIf lstWin2.Items.Contains(1) And lstWin2.Items.Contains(5) And lstWin2.Items.Contains(9) Then
+            DrawGraphics.DrawLine(MyPen, 10, 10, 310, 310)
+            MsgBox("Player 2 wins")
+        ElseIf _intDraws = 9 Then
+            MsgBox("No Winner")
+        End If
+
+    End Sub
+
+    Private Function MousePos(e As MouseEventArgs) As Integer
+        Dim intSquare As Integer
+        If e.X < 110 And e.X > 10 Then
+            If e.Y < 110 And e.Y > 10 Then
+                DetermineSquare(0, 0)
+                intSquare = 1
+            ElseIf e.Y < 210 And e.Y > 110 Then
+                DetermineSquare(0, 100)
+                intSquare = 2
+            ElseIf e.Y < 310 And e.Y > 210 Then
+                DetermineSquare(0, 200)
+                intSquare = 3
+            End If
+        ElseIf e.X < 210 And e.X > 110 Then
+            If e.Y < 110 And e.Y > 10 Then
+                DetermineSquare(100, 0)
+                intSquare = 4
+            ElseIf e.Y < 210 And e.Y > 110 Then
+                DetermineSquare(100, 100)
+                intSquare = 5
+            ElseIf e.Y < 310 And e.Y > 210 Then
+                DetermineSquare(100, 200)
+                intSquare = 6
+            End If
+        ElseIf e.X < 310 And e.X > 210 Then
+            If e.Y < 110 And e.Y > 10 Then
+                DetermineSquare(200, 0)
+                intSquare = 7
+            ElseIf e.Y < 210 And e.Y > 110 Then
+                DetermineSquare(200, 100)
+                intSquare = 8
+            ElseIf e.Y < 310 And e.Y > 210 Then
+                DetermineSquare(200, 200)
+                intSquare = 9
+            End If
+        ElseIf (e.Y > 310 And e.Y < 10) Then
+            If (e.X > 310 And e.X < 10) Then
+                intSquare = 0
+            End If
+            intSquare = 0
+        End If
+        Return intSquare
+    End Function
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ResetBoard()
+        _intDraws = 0
+    End Sub
+End Class
